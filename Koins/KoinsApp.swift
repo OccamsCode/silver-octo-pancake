2e51961387822724662ccbf782ae0885e1e020aa
Brian M <b.a.munjoma@gmail.com>
2023-10-11 16:18:21 +0100

    Initial Commit

---
+++ Koins/KoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoinsApp.swift
+//  Koins
+//
+//  Created by Brian Munjoma on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
